769738156c0352e8ee442d5e598d3087cad57a40
Monakus <monakus00@gmail.com>
2017-05-29 15:29:33 +0200

    Pierwszy commit
    Zaczyna się prawdziwa przygoda

---
+++ HackAndSlash2D/Assets/Skrypty/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+	public GameObject gracz;
+	public float reachDist = 0.1f;
+	public float speed; 
+
+	void Start(){
+		gracz = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void Update(){
+		
+		float distance = Vector3.Distance (gracz.transform.position, transform.position);
+
+		if (distance > reachDist) {
+			transform.position = Vector3.MoveTowards (transform.position, gracz.transform.position, speed * Time.deltaTime);
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D other){
+		if (other.gameObject.tag == "Broń") {
+			Destroy (gameObject);
+		}
+	}
+}
+
---
+++ HackAndSlash2D/Assets/Skrypty/Spawn.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+	public Transform potwór;
+	public int numerFali = 1;
+	public bool koniecFali;
+	public int iloscPotworów, zespawnionePotwory;
+
+	public float timer, zmianaTimera = 0.1f, nowyTimer = 2f;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		if (koniecFali == true){
+			timer -= Time.deltaTime;
+			if (timer <= 0f)
+			{
+				Instantiate (potwór, new Vector2 (transform.position.x, transform.position.y), Quaternion.identity);
+				zespawnionePotwory++;
+				timer = nowyTimer;
+			}
+		}
+
+		if (zespawnionePotwory == numerFali * 10) {
+			koniecFali = false;
+			numerFali++;
+			zespawnionePotwory = 0;
+			nowyTimer = timer - zmianaTimera;
+		}
+
+		iloscPotworów = GameObject.FindGameObjectsWithTag ("Potwór").Length;
+
+		if (iloscPotworów == 0 && zespawnionePotwory == 0)
+			koniecFali = true;
+
+	}
+}
+
+
+
