75966b25faaa2733c0343e5c98b0757acbc438eb
Monakus <monakus00@gmail.com>
2017-05-29 20:02:16 +0200

    Nowe poruszanie

--- HackAndSlash2D/Assets/Skrypty/PlayerMovement.cs
+++ HackAndSlash2D/Assets/Skrypty/PlayerMovement.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 	public float moveSpeed;
 	private GameManager gameManagerSkrypt;
+	private Rigidbody2D rb;
+	private Vector2 movement;
+	public Vector2 speed = new Vector2(5, 5);
 
 	//private Animator anim;
 
 	void Start () {
 		//anim = GetComponent<Animator> ();
+		rb = GetComponent<Rigidbody2D>();
 
 		GameObject obiekt = GameObject.Find ("GameManager");
 		gameManagerSkrypt = obiekt.GetComponent<GameManager> ();
@@ -18,18 +22,30 @@ public class PlayerMovement : MonoBehaviour {
 	}
 
 	void Update () {
-		transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0f);
 
-		if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < 0.5f){
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+
+		movement = new Vector2(
+			speed.x * inputX,
+			speed.y * inputY);
+
+		/*if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < 0.5f){
 			transform.Translate (new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
 		}
 
 		if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < 0.5f){
 			transform.Translate (new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
-		}
+		}*/
 
 		/*anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
 		anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));*/
+	}
+
+	void FixedUpdate(){
+		transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0.0f);
+		rb.velocity = movement;
+
 	}
 
 	void OnTriggerEnter2D (Collider2D other){
